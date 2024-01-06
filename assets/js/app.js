c641e80c5413f5ce0166c2b51a5a22dd12491299
Slavik <aviewhub@gmail.com>
2024-01-06 23:43:53 +0100

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -179,7 +179,7 @@ console.log(serialize({
 }));
 
 function submitEmail(){
-  window.location = "mailto:contact@canaveralsolutions.ltd" + serialize({
+  window.location = "mailto:contact@canaveralsolutions.ltd?" + serialize({
     subject: document.getElementById('subject').value,
     body: "My name : " + document.getElementById('name').value + "\n" + "My email : " + document.getElementById('email').value + "\n" + document.getElementById('comments').value
   });
